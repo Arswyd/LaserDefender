da4e070cfa103b7d3fa72e968032ff3a012a34ae
Varsanyi\Varsányi Dániel <vdani94@gmail.com>
2022-03-29 21:37:58 +0200

    Added Enemy spawner, enemy waves and pathfinding

---
+++ Assets/Scripts/WaveConfigSO.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Wave Config", fileName = "New Wave Config")]
+public class WaveConfigSO : ScriptableObject
+{
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] Transform pathPrefab;
+    [SerializeField] List<GameObject> enemyPrefabs;
+    [SerializeField] float timeBetweenEnemySpawns = 1f;
+    [SerializeField] float spawnTimeVariance = 0f;
+    [SerializeField] float minimumSpawnTime = 0.2f;
+
+
+    public float GetMoveSpeed()
+    {
+        return moveSpeed;
+    }
+
+    public Transform GetStartingWaypoint()
+    {
+        return pathPrefab.GetChild(0);
+    }
+
+    public List<Transform> GetWaypoints()
+    {
+        List<Transform> waypoints = new List<Transform>();
+
+        foreach(Transform waypoint in pathPrefab)
+        {
+            waypoints.Add(waypoint);
+        }
+
+        return waypoints;
+    }
+
+    public int GetEnemyCount()
+    {
+        return enemyPrefabs.Count;
+    }
+
+    public GameObject GetEnemyPrefab(int index)
+    {
+        return enemyPrefabs[index];
+    }
+
+    public float GetRandomSpawnTime()
+    {
+        float spawnTime = Random.Range(timeBetweenEnemySpawns - spawnTimeVariance, timeBetweenEnemySpawns + spawnTimeVariance);
+        return Mathf.Clamp(spawnTime, minimumSpawnTime, float.MaxValue);
+    }
+}
